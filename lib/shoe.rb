d52879279010dfe7e3786c2d8e13eaec84ebb558
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-20 04:35:29 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,12 +3,9 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
  
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
  
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
  
 end
